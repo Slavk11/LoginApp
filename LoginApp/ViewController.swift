d84f64584ce6ba17f078ae1234022b521f47338c
Slavk <slavk11@yandex.ru>
2023-03-31 22:31:14 +0300

    released prompt buttons text

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class LoginViewController: UIViewController {
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       
     }
 
 
+    @IBAction func logInButtonTapped() {
+       // guard let inputText = userNameTextField.text
+        let userNamePattern = "login"
+        let isUserNameValid = NSPredicate(format: "SELF MATCHES %@", userNamePattern).evaluate(with: userNameTextField)
+        if !isUserNameValid {
+            return
+        }
+    }
+    
+    
+    @IBAction private func promptLoginButtonPressed() {
+       showAlert(withTitle: "Just forgot your login? 😄", andMessage: "Your login is - login")
+    }
+    
+    @IBAction private func promptPasswordButtonPressed() {
+        showAlert(withTitle: "Is it really you?! 🥹", andMessage: "Ok... Your password is - password")
+    }
+    
+    
+    
+    
+     private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+         let okAction = UIAlertAction(title: "OK", style: .default)
+         alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
