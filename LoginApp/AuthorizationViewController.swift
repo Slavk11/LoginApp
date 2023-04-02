d1d47e26e4cf1cca0bfe9daf8aea6cee5f16b974
Slavk <slavk11@yandex.ru>
2023-04-02 09:28:18 +0300

    minor fixes

--- LoginApp/AuthorizationViewController.swift
+++ LoginApp/AuthorizationViewController.swift
@@ -11,15 +11,23 @@ final class AuthorizationViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    @IBAction func unwind(for segue: UIStoryboardSegue) {
-        userNameTextField.text = ""
-        passwordTextField.text = ""
+    @IBOutlet var logInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        logInButton.layer.cornerRadius = 5
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let userProfileVC = segue.destination as? UserProfileViewController else {return}
-        userProfileVC.message = "Welcome, \(userNameTextField.text ?? "")!"
-   }
+        userProfileVC.greetingMessage = "Welcome, \(userNameTextField.text ?? "")!"
+    }
+    
+    @IBAction private func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
     
     @IBAction private func logInButtonTapped() {
         let inputLogin = userNameTextField.text
@@ -31,24 +39,24 @@ final class AuthorizationViewController: UIViewController {
         let isUserNameValid = NSPredicate(format: "SELF MATCHES %@", userNamePattern).evaluate(with: inputLogin)
         let isUserPasswordValid = NSPredicate(format: "SELF MATCHES %@", userPasswordPattern).evaluate(with: inputPassword)
         if !isUserNameValid || !isUserPasswordValid {
-            showAlert(withTitle: "Is it really you?! 🥹", andMessage: "Password or login not correct")
+            showAlert(withTitle: "Is it really you?! 🥹", andMessage: "Login or password not correct")
         }
     }
     
     @IBAction private func promptLoginButtonPressed() {
-       showAlert(withTitle: "Just forgot your login? 😄", andMessage: "Your login is - Admin")
+        showAlert(withTitle: "Just forgot your login?", andMessage: "Your login is - Admin")
     }
     
     @IBAction private func promptPasswordButtonPressed() {
-        showAlert(withTitle: "Is it really you?! 🥹", andMessage: "Ok... Your password is - password")
+        showAlert(withTitle: "Just forgot your password?", andMessage: "Ok... Your password is - password")
     }
     
-     private func showAlert(withTitle title: String, andMessage message: String) {
+    private func showAlert(withTitle title: String, andMessage message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-             self.passwordTextField.text = ""
-         }
-         alert.addAction(okAction)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTextField.text = ""
+        }
+        alert.addAction(okAction)
         present(alert, animated: true)
     }
 }
